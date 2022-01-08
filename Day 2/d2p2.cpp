4109bba406104b403a2b8816c2c8351253a62cfb
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-01-08 00:32:10 -0600

    general update

--- Day 2/d2p2.cpp
+++ Day 2/d2p2.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    ifstream infile ("day2.txt");
+    string buffer;
+    int val;
+    int x=0, y=0;
 
+    if (infile.bad()) {
+        cout << "[-] Could not open file...\n";
+        exit(-1);
+    }
+
+    while (infile >> buffer >> val) {
+        switch (buffer[0]) {
+            case 'f': // forward
+                x *= val;
+                break;
+            case 'u': // up
+                y -= val;
+                break;
+            case 'd': // down
+                y += val;
+                break;
+        }
+    }
+
+    cout << (x*y) << endl;
     return 0;
 }
