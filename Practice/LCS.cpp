7e29f42c08a0d0729c2eea36f945329465ac2485
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-01-09 16:23:19 -0600

    Complete

--- Practice/LCS.cpp
+++ Practice/LCS.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,9 +9,11 @@ int LCSLength(string x, string y) {
 
     int lookup[m + 1][n + 1];
 
-    for (int i=0; i<=m; i++) {
-        lookup[0][i] = 0;
-        lookup[i][0] = 0;
+    for (int i=0; i <= m || i <= n; i++) {
+        if (i <= n)
+            lookup[0][i] = 0;
+        if (i <= m)
+            lookup[i][0] = 0;
     }
 
     for (int i=1; i<=m; i++) {
@@ -26,11 +29,54 @@ int LCSLength(string x, string y) {
     return lookup[m][n];
 }
 
+string LCSSequence(string x, string y) {
+    int m = x.length(), n = y.length();
+
+    int lookup[m + 1][n + 1];
+    string retval = "";
+
+    for (int i=0; i <= m || i <= n; i++) {
+        if (i <= n)
+            lookup[0][i] = 0;
+        if (i <= m)
+            lookup[i][0] = 0;
+    }
+
+    for (int i=1; i<=m; i++) {
+        for (int j=1; j<=n; j++) {
+
+            if (x[i-1] == y[j-1]) {
+                lookup[i][j] = lookup[i - 1][j - 1] + 1;
+            } else {
+                lookup[i][j] = max(lookup[i][j-1], lookup[i-1][j]);
+            }
+        }
+    }
+    
+    int index = lookup[m][n];
+    char lcs[index+1];
+    lcs[index] = '\0';
+
+    int i=m, j=n;
+    while (i > 0 && j > 0) {
+        if (x[i-1] == y[j-1]) {
+            lcs[index-1] = x[i-1];
+            i--; j--; index--;
+        }
+        else if (lcs[i-1] > lcs[j-1])
+            i--;
+        else
+            j--;
+    }
+    return lcs;
+}
+
 int main()
 {
-    string X = "abce", Y = "abcde";
+    string X = "xyz", Y = "xaz";
  
     cout << "The length of the LCS is " << LCSLength(X, Y) << endl;
+    cout << "The LCS is " << LCSSequence(X, Y) << endl; // only prints first one
  
     return 0;
 }
